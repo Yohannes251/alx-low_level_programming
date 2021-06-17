4fb03460f9a2695016828854ac2bc826dab6f245
Yohannes251 <yohannes.z251@gmail.com>
2021-06-17 08:09:45 -0700

    Prints a string with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Return: returns 0 if the function is executed properly
+ */
+int main(void)
+{
+	char *str = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+	return (0);
+}
