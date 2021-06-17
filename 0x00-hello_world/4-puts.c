d8257ab26f009d9c7b29ac7fc39b4aebc08cd27f
Yohannes251 <yohannes.z251@gmail.com>
2021-06-17 07:55:59 -0700

    C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * Prints a string
+ * main - Prints a string
+ * Return: returns 0 if the function is
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(str);
 	return (0);
 }
