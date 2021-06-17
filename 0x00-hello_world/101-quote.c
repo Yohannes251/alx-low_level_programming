14bf9098ac79293490fdd4e85c196e68b9ce4dc4
Yohannes251 <yohannes.z251@gmail.com>
2021-06-17 11:39:46 -0700

    Print a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the standard error
+ * Return: Returns 1 if the code executes
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
