6e2f508aea6b896ee0757be74d9447ae84cf5c8f
Yohannes251 <yohannes.z251@gmail.com>
2021-06-18 06:45:18 -0700

    Declare a random number as positive negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function dictates whether a number is +ve,-ve or 0
@@ -11,7 +12,7 @@ int main(void)
 	n = rand();
 	if (n > 0)
 		printf("%d is positive\n", n);
-	elif (n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
