fa9c7909ad7e7db9b45584fa7077ad37780cbfe0
Yohannes251 <yohannes.z251@gmail.com>
2021-06-22 06:52:06 -0700

    Prints the alphabet in lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y = 0;
+
+	while (y < 10)
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+	_putchar('\n');
+}
