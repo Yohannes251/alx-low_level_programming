35a40b6c9f831791c0c08683ba7163ec83a2b0b4
Yohannes251 <yohannes.z251@gmail.com>
2021-06-22 13:02:52 -0700

    Adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * add - Adds two values
+  * @x: The first integer
+  * @y: The second integer
+  * Return: returns the sum of the integers
+  */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
