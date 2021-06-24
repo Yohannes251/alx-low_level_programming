487c2f6279f5a93e012125c51da5e3f0bff247f2
Yohannes251 <yohannes.z251@gmail.com>
2021-06-24 08:43:40 -0700

    Prints numbers 1 to 100 and assigns words to some numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ * Multiples of 3 will be named Fizz
+ * Multiples of 5 will be named Buzz
+ * Multiples of both 3 & 5 will be named FizzBuzz
+ * Return: Returns 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
