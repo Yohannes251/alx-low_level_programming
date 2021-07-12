05daeec6c2885d005d9e5f3834ea2212defa35f2
Yohannes251 <yohannes.z251@gmail.com>
2021-07-12 11:29:49 -0700

    Multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: pointer to string array
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int result;
+
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+}
